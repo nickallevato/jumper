11820f2b3314dc20e57a3b1bff59bf55138d833a
nick allevato <nick@allevto.io>
2026-05-19 02:45:16 -0600

    feat: project scaffold, deps, shared constants

---
+++ shared/constants.js
@@ -0,0 +1,30 @@
+export const TICK_MS = 50             // 20 ticks/sec
+export const TILE_W = 64
+export const TILE_H = 32
+export const MOVE_SPEED = 4           // tiles per second
+export const JUMP_VELOCITY = 0.4      // tiles/tick upward
+export const GRAVITY = 0.03           // tiles/tick² downward
+export const ROOM_CAP_SMALL = 6
+export const ROOM_CAP_DUNGEON = 50
+
+export const ITEM_EFFECTS = {
+  floaty_jump:    { gravity: 0.015 },
+  high_jump:      { jumpVelocity: 0.7 },
+  reveal_hidden:  { revealHidden: true },
+}
+
+export const SOCKET_EVENTS = {
+  AUTH:          'auth',
+  AUTH_OK:       'auth:ok',
+  JOIN_ROOM:     'join:room',
+  JOIN_OK:       'join:ok',
+  ROOM_DENIED:   'room:denied',
+  MOVE:          'move',
+  TICK:          'tick',
+  ITEM_PICKUP:   'item:pickup',
+  ITEM_DROP:     'item:drop',
+  ITEM_USE:      'item:use',
+  ITEM_STATE:    'item:state',
+  DISCOVER:      'discover',
+  DISCOVER_OK:   'discover:ok',
+}
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: 'client',
+  build: { outDir: '../dist', emptyOutDir: true },
+  server: {
+    proxy: {
+      '/socket.io': { target: 'http://localhost:3000', ws: true },
+      '/api': 'http://localhost:3000',
+    },
+  },
+})
